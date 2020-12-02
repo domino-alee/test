6585e3ee9eb186a030fbeeb48aa256b1069eccd4
domino-alee <48601396+domino-alee@users.noreply.github.com>
2020-12-02 16:09:51 -0600

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,5 +1,5 @@
 whoami; ls -latr /opt/bitnami/spark/conf; cat /opt/test/conf/spark-defaults.conf
-RUN mkdir /opt/test && cd /opt/test && cp -r /opt/bitnami/spark/conf/ . && chmod -R 777 /opt/test
+mkdir /opt/test && cd /opt/test && cp -r /opt/bitnami/spark/conf/ . && chmod -R 777 /opt/test
 echo "spark.authenticate true" >> /opt/test/conf/spark-defaults.conf
 echo "spark.authenticate.secret Domino123!@#" >> /opt/test/conf/spark-defaults.conf
 echo "spark.network.crypto.enabled true" >> /opt/test/conf/spark-defaults.conf
