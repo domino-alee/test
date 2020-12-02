90023150d60b264d5816b13bcd51af2d60051cd3
domino-alee <48601396+domino-alee@users.noreply.github.com>
2020-12-02 16:08:50 -0600

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,3 +1,10 @@
-whoami; ls -latr /opt/bitnami/spark/conf
-chmod -R 777 /opt/bitnami/spark/conf; chmod -R 777 /opt/bitnami/spark/python/test_coverage/conf
-echo "spark.authenticate true" >> /opt/bitnami/spark/conf/spark-defaults.conf; echo "spark.authenticate true" >> /opt/bitnami/spark/python/test_coverage/conf/spark-defaults.conf; echo "spark.authenticate.secret Domino123!@#" >> /opt/bitnami/spark/conf/spark-defaults.conf; echo "spark.authenticate.secret Domino123!@#" >> /opt/bitnami/spark/python/test_coverage/conf/spark-defaults.conf; echo "spark.network.crypto.enabled true" >> /opt/bitnami/spark/conf/spark-defaults.conf; echo "spark.network.crypto.enabled true" >> /opt/bitnami/spark/python/test_coverage/conf/spark-defaults.conf; echo "spark.io.encryption.enabled true" >> /opt/bitnami/spark/conf/spark-defaults.conf; echo "spark.io.encryption.enabled true" >> /opt/bitnami/spark/python/test_coverage/conf/spark-defaults.conf; echo "spark.authenticate.enableSaslEncryption true" >> /opt/bitnami/spark/conf/spark-defaults.conf; echo "spark.authenticate.enableSaslEncryption true" >> /opt/bitnami/spark/python/test_coverage/conf/spark-defaults.conf
+whoami; ls -latr /opt/bitnami/spark/conf; cat /opt/test/conf/spark-defaults.conf
+RUN mkdir /opt/test && cd /opt/test && cp -r /opt/bitnami/spark/conf/ . && chmod -R 777 /opt/test
+echo "spark.authenticate true" >> /opt/test/conf/spark-defaults.conf
+echo "spark.authenticate.secret Domino123!@#" >> /opt/test/conf/spark-defaults.conf
+echo "spark.network.crypto.enabled true" >> /opt/test/conf/spark-defaults.conf
+echo "spark.io.encryption.enabled true" >> /opt/test/conf/spark-defaults.conf
+echo "spark.authenticate.enableSaslEncryption true" >> /opt/test/conf/spark-defaults.conf
+cat /opt/test/conf/spark-defaults.conf
+export SPARK_CONF_DIR=/opt/test/conf
+env | grep SPARK
