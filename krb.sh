02afd79dd77925d5c4cbe57e13a2953116d7a7c4
domino-alee <48601396+domino-alee@users.noreply.github.com>
2020-12-08 10:24:07 -0600

    Update krb.sh

--- krb.sh
+++ krb.sh
@@ -1,4 +1,5 @@
 mkdir /opt/test && cd /opt/test && cp -r /opt/bitnami/spark/conf/ . && wget https://github.com/domino-alee/test/raw/master/keystore.jks && wget https://github.com/domino-alee/test/raw/master/truststore.jks && chmod -R 777 /opt/test
+cd /etc && wget https://raw.githubusercontent.com/domino-alee/test/master/krb5.conf && wget https://github.com/domino-alee/test/raw/master/spark.headless.keytab && wget https://github.com/domino-alee/test/raw/master/spark.service.keytab && chmod 777 *keytab && chmod 777 krb5.conf
 echo "spark.kerberos.access.hadoopFileSystems hdfs://ec2-107-20-103-193.compute-1.amazonaws.com:8020" >> /opt/test/conf/spark-defaults.conf
 echo "spark.kerberos.keytab /etc/spark.service.keytab" >> /opt/test/conf/spark-defaults.conf
 echo "spark.kerberos.principal spark/ip-10-99-121-113.ec2.internal@EC2.INTERNAL" >> /opt/test/conf/spark-defaults.conf
