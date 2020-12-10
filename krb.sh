913748fe2d209b9d24b5ca614b4b1309a6ab934d
domino-alee <48601396+domino-alee@users.noreply.github.com>
2020-12-09 21:01:05 -0600

    Update krb.sh

--- krb.sh
+++ krb.sh
@@ -5,7 +5,9 @@ echo "spark.kerberos.access.hadoopFileSystems hdfs://ec2-107-20-103-193.compute-
 echo "spark.kerberos.keytab /opt/spark.service.keytab" >> /opt/test/conf/spark-defaults.conf
 echo "spark.kerberos.principal spark/ip-10-99-121-113.ec2.internal@EC2.INTERNAL" >> /opt/test/conf/spark-defaults.conf
 echo "SPARK_CONF_DIR=/opt/test/conf" >> /opt/test/conf/spark-env.sh
+echo "HADOOP_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0" >> /opt/test/conf/spark-env.sh
 echo "SPARK_CONF_DIR=/opt/test/conf" >> /opt/bitnami/spark/conf/spark-env.sh
+echo "HADOOP_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0" >> /opt/bitnami/spark/conf/spark-env.sh
 export SPARK_CONF_DIR=/opt/test/conf
 export HADOOP_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0
 env
