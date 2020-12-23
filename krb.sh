d0a1a1c24b7ccecd2c94d17029c3737338967642
domino-alee <48601396+domino-alee@users.noreply.github.com>
2020-12-22 20:55:21 -0600

    Update krb.sh

--- krb.sh
+++ krb.sh
@@ -6,26 +6,26 @@ cd /opt && wget https://github.com/domino-alee/test/raw/master/hdpkerberos.tar.g
 #echo "spark.kerberos.access.hadoopFileSystems hdfs://ec2-107-20-103-193.compute-1.amazonaws.com:8020" >> /opt/test/conf/spark-defaults.conf
 echo "spark.kerberos.keytab /opt/spark.service.keytab" >> /opt/test/conf/spark-defaults.conf
 echo "spark.kerberos.principal spark/ip-10-99-121-113.ec2.internal@EC2.INTERNAL" >> /opt/test/conf/spark-defaults.conf
-echo "java.security.auth.login.config /opt/jaas.conf" >> /opt/test/conf/spark-defaults.conf
+#echo "java.security.auth.login.config /opt/jaas.conf" >> /opt/test/conf/spark-defaults.conf
 echo "spark.executorEnv.HADOOP_TOKEN_FILE_LOCATION /opt/spark.token" >> /opt/test/conf/spark-defaults.conf
-echo "spark.executor.extraJavaOptions -Djava.security.auth.login.config=/opt/jaas.conf" >> /opt/test/conf/spark-defaults.conf
+#echo "spark.executor.extraJavaOptions -Djava.security.auth.login.config=/opt/jaas.conf" >> /opt/test/conf/spark-defaults.conf
 
 #echo "HDFS_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0" >> /opt/bitnami/spark/conf/spark-env.sh
 echo "SPARK_CONF_DIR=/opt/test/conf" >> /opt/bitnami/spark/conf/spark-env.sh
 echo "HADOOP_TOKEN_FILE_LOCATION=/opt/spark.token" >> /opt/bitnami/spark/conf/spark-env.sh
-echo "SPARK_DAEMON_JAVA_OPTS=\"-Djava.security.auth.login.config=/opt/jaas.conf -Djava.security.krb5.conf=/etc/krb5.conf\"" >> /opt/bitnami/spark/conf/spark-env.sh
+#echo "SPARK_DAEMON_JAVA_OPTS=\"-Djava.security.auth.login.config=/opt/jaas.conf -Djava.security.krb5.conf=/etc/krb5.conf\"" >> /opt/bitnami/spark/conf/spark-env.sh
 echo "HADOOP_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0" >> /opt/bitnami/spark/conf/spark-env.sh
 
 #echo "HDFS_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0" >> /opt/test/conf/spark-env.sh
 echo "SPARK_CONF_DIR=/opt/test/conf" >> /opt/test/conf/spark-env.sh
 echo "HADOOP_TOKEN_FILE_LOCATION=/opt/spark.token" >> /opt/test/conf/spark-env.sh
-echo "SPARK_DAEMON_JAVA_OPTS=\"-Djava.security.auth.login.config=/opt/jaas.conf -Djava.security.krb5.conf=/etc/krb5.conf\"" >> /opt/test/conf/spark-env.sh
+#echo "SPARK_DAEMON_JAVA_OPTS=\"-Djava.security.auth.login.config=/opt/jaas.conf -Djava.security.krb5.conf=/etc/krb5.conf\"" >> /opt/test/conf/spark-env.sh
 echo "HADOOP_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0" >> /opt/test/conf/spark-env.sh
 
 #export HDFS_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0
 export SPARK_CONF_DIR=/opt/test/conf
 export HADOOP_TOKEN_FILE_LOCATION=/opt/spark.token
-export SPARK_DAEMON_JAVA_OPTS="-Djava.security.auth.login.config=/opt/jaas.conf -Djava.security.krb5.conf=/etc/krb5.conf"
+#export SPARK_DAEMON_JAVA_OPTS="-Djava.security.auth.login.config=/opt/jaas.conf -Djava.security.krb5.conf=/etc/krb5.conf"
 export HADOOP_CONF_DIR=/opt/hadoop-binaries-configs/configs/hadoop/3.1.4.0-315/0
 
 env
